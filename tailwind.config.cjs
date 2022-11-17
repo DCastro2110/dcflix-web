1eb5523aa86fa2c4ca091787e9411944d5207d87
Davi Castro <santos.davi2110@gmail.com>
2022-11-17 10:05:27 -0300

    fix: error of tailwind config was fixed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/*.{js,ts,tsx,jsx}", "./src/*/*/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,tsx,jsx}"],
   theme: {
     extend: {
       colors: {
