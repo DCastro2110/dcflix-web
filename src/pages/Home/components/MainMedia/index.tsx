01703112a03201b57f31970bba0ade5b6ae07243
Davi Castro <santos.davi2110@gmail.com>
2022-11-18 09:25:04 -0300

    fix: title of main media size in mobile devices was changed

--- src/pages/Home/components/MainMedia/index.tsx
+++ src/pages/Home/components/MainMedia/index.tsx
@@ -6,8 +6,8 @@ export function MainMedia() {
       className={`relative h-[80vh] bg-white/40 text-white bg-cover bg-center bg-[url('/images/main-bg.jpeg')]`}>
       <div className="absolute inset-0 bg-main-gradient-bottom">
         <div className="h-full container max-w-[1600px] flex items-center">
-          <div className="space-y-4 -mt-24 ">
-            <h1 className="font-bold text-6xl">The Wonder</h1>
+          <div className="space-y-4 md:-mt-24 ">
+            <h1 className="font-bold text-4xl md:text-6xl">The Wonder</h1>
 
             <p className="max-w-md">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. At iusto
