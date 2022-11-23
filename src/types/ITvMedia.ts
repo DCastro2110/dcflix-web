a7d14cc059d9de4515453b1d3a342432c7d13884
Davi Castro <davicastro.dev@gmail.com>
2022-11-23 08:17:15 -0300

    chore: tv and movie types was added

---
+++ src/types/ITvMedia.ts
@@ -0,0 +1,86 @@
+import { IGenre } from './IGenre';
+import { IProductionCompany } from './IProductionCompany';
+import { IProductionCountry } from './IProductionCountry';
+import { ISpokenLanguage } from './ISpokenLanguage ';
+
+interface LastEpisodeToAir {
+  air_date: string;
+  episode_number: number;
+  id: number;
+  name: string;
+  overview: string;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface NextEpisodeToAir {
+  air_date: string;
+  episode_number: number;
+  id: number;
+  name: string;
+  overview: string;
+  production_code: string;
+  runtime?: any;
+  season_number: number;
+  show_id: number;
+  still_path?: any;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface Network {
+  id: number;
+  name: string;
+  logo_path: string;
+  origin_country: string;
+}
+
+interface Season {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  season_number: number;
+}
+
+export interface ITvMedia {
+  adult: boolean;
+  backdrop_path: string;
+  created_by: any[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: IGenre[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: LastEpisodeToAir;
+  name: string;
+  next_episode_to_air: NextEpisodeToAir;
+  networks: Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: IProductionCompany[];
+  production_countries: IProductionCountry[];
+  seasons: Season[];
+  spoken_languages: ISpokenLanguage[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
