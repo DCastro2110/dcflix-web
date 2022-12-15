82c2c1c7b3015156e5d4ea3d4b6fad2892edd39e
Davi Castro <santos.davi2110@gmail.com>
2022-12-15 10:30:52 -0300

    refactor: router method was changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
 import './styles/global.css';
 
-import { AppRoutes } from './routers/app.routes';
+import { AppRoutes } from './routers/App.routes';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={AppRoutes} />
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   </QueryClientProvider>
 );
---
+++ src/routers/App.routes.tsx
@@ -0,0 +1,99 @@
+import { ReactElement, useContext } from 'react';
+import { Routes, Route, useParams, Navigate } from 'react-router-dom';
+import { AuthContextProvider, AuthContext } from '../contexts/AuthContext';
+
+import {
+  Home,
+  Browse,
+  About,
+  Search,
+  Filter,
+  Play,
+  SignIn,
+  SignUp,
+} from '../pages';
+
+interface IPrivateRouteProps {
+  children: ReactElement;
+}
+
+function FilterWrapper() {
+  const { slug } = useParams();
+
+  return <Filter key={slug} />;
+}
+
+function PrivateRoute({ children }: IPrivateRouteProps) {
+  const { user } = useContext(AuthContext);
+
+  if (user.id) {
+    return children;
+  }
+
+  return <Navigate to="/" />;
+}
+
+export function AppRoutes() {
+  return (
+    <AuthContextProvider>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/signin"
+          element={<SignIn />}
+        />
+        <Route
+          path="/signup"
+          element={<SignUp />}
+        />
+        <Route
+          path="/browse"
+          element={
+            <PrivateRoute>
+              <Browse />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="about/:id"
+          element={
+            <PrivateRoute>
+              <About />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="play/:id"
+          element={
+            <PrivateRoute>
+              <Play />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <PrivateRoute>
+              <Search />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/filter/:slug"
+          element={
+            <PrivateRoute>
+              <FilterWrapper />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="*"
+          element={<Navigate to="browse" />}
+        />
+      </Routes>
+    </AuthContextProvider>
+  );
+}
